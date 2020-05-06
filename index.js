435c23237ba24730280a37af079c3d43be05acf1
Sergey Zwezdin <sergey@zwezdin.com>
2020-05-06 13:02:45 +0500

    feat: Resolve image helper and templates for tag helper added

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+const path = require('path');
+
 hexo.config.images = Object.assign(
     {
         enable: true,
         base_dir: '.images',
         manifestFileName: 'images.json',
         excludeSourceImages: true,
-        specialImages: []
+        specialImages: [],
+        templates: {
+            image: path.relative(hexo.base_dir, path.resolve(__dirname, 'lib', 'templates', 'image.ejs')),
+            video: path.relative(hexo.base_dir, path.resolve(__dirname, 'lib', 'templates', 'video.ejs'))
+        }
     },
     hexo.config.images
 );
@@ -23,7 +29,10 @@ if (hexo.config.images.enable) {
 
     // Inject image cache manifest into page metadata
     hexo.extend.filter.register('before_post_render', require('./lib/postMetadataFilter')(hexo));
-}
 
-// Helper to resolve image by name from the current post context
-hexo.extend.helper.register('resolve_image', require('./lib/resolveImageHelper')(hexo));
+    // Helper to resolve image by name from the current post context
+    hexo.extend.helper.register('resolve_image', require('./lib/resolveImageHelper')(hexo));
+
+    // Picture tag
+    hexo.extend.tag.register('picture', require('./lib/picture')(hexo), { ends: true });
+}
