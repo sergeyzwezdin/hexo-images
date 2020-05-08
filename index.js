99f65ec81eda72a0b3275a9e8c572c473c0bd792
Sergey Zwezdin <sergey@zwezdin.com>
2020-05-08 12:48:19 +0500

    feat: Image invalidation update

--- index.js
+++ index.js
@@ -16,8 +16,37 @@ hexo.config.images = Object.assign(
 );
 
 if (hexo.config.images.enable) {
-    // Process source images, write to cache folder
-    hexo.extend.filter.register('before_generate', require('./lib/imagesInvalidateFilter')(hexo));
+    /**
+     * Process source images and write to cache folder
+     * when post content or file(s) changed - what happens first.
+     *
+     * ⚠️ Note: if images already validated in the current cycle,
+     * it won't be runned again until "after_generate".
+     */
+    const invalidateImages = require('./lib/invalidateImages')(hexo);
+    const injectMetadata = require('./lib/injectMetadata')(hexo);
+    let imagesInvaidated = false;
+
+    hexo.extend.filter.register('before_generate', function () {
+        if (imagesInvaidated === false) {
+            imagesInvaidated = true;
+            return invalidateImages();
+        }
+    });
+
+    // Inject image cache manifest into page metadata. Process images before if needed.
+    hexo.extend.filter.register('before_post_render', function (data) {
+        if (imagesInvaidated === false) {
+            imagesInvaidated = true;
+            return invalidateImages().then(() => injectMetadata(data));
+        } else {
+            return injectMetadata(data);
+        }
+    });
+
+    hexo.extend.filter.register('after_generate', function () {
+        imagesInvaidated = false;
+    });
 
     // Exclude source images from output
     if (hexo.config.images.excludeSourceImages) {
@@ -26,9 +55,6 @@ if (hexo.config.images.enable) {
 
     // Inlcude processed images into final output
     hexo.extend.generator.register('images', require('./lib/imagesGenerator')(hexo));
-
-    // Inject image cache manifest into page metadata
-    hexo.extend.filter.register('before_post_render', require('./lib/postMetadataFilter')(hexo));
 
     // Helper to resolve image by name from the current post context
     hexo.extend.helper.register('resolve_image', require('./lib/resolveImageHelper')(hexo));
